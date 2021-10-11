3d839f4ec7d1a11dfc5cfb69433026b2228d73dd
Laurentiu <lcostin40@gmail.com>
2021-10-11 16:34:09 +0200

    Prototipo di calcolo del prezzo in base ai kilometri scelti e prezzo finale.

--- main.js
+++ main.js
@@ -16,4 +16,15 @@ console.log(Number(userAge));
 //Calcoliamo il prezzo del biglietto in base ai kilometri scelti
 
 
+x = 0.21;
 
+const prezzo = kilometri * x;
+
+console.log(prezzo);
+
+// Calcola il prezzo finale del biglietto in base all'età
+y = 0.2;
+
+const prezzo_finale = prezzo * y;
+
+console.log(prezzo_finale);
