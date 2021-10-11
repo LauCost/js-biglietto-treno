adcf578d275c7f08eeed4c6de89da30bb92f7106
Laurentiu <lcostin40@gmail.com>
2021-10-11 16:48:11 +0200

    Prototipo per calcolare il prezzo finale del biglietto considerando anche l'età

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //Chiede il numero di km da voler percorrere
 
-let kilometri = prompt("Quanti kilometri vuoi percorrere?");
+const kilometri = prompt("Quanti kilometri vuoi percorrere?");
 
 /* Number(kilometri); */
 
@@ -8,23 +8,33 @@ console.log(Number(kilometri));
 
 //Chiede l'età all'utente
 
-let userAge = prompt("Quanti anni hai?");
+const userAge = prompt("Quanti anni hai?");
 
 console.log(Number(userAge));
 
 
 //Calcoliamo il prezzo del biglietto in base ai kilometri scelti
 
-
 x = 0.21;
+
+y = 0.2;
+
+z = 0.4;
 
 const prezzo = kilometri * x;
 
-console.log(prezzo);
 
 // Calcola il prezzo finale del biglietto in base all'età
-y = 0.2;
 
-const prezzo_finale = prezzo * y;
+let prezzo_finale
+console.log("Solo kilometri: " + prezzo);
 
-console.log(prezzo_finale);
+if (userAge > 65) {
+    prezzo_finale = prezzo * z;
+} else if (userAge < 18) {
+    prezzo_finale = prezzo * y;
+} else {
+    prezzo_finale = prezzo;
+}
+
+console.log("Il prezzo del biglietto è " + prezzo_finale);
